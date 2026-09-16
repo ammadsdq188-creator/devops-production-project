47881434a1f2621514e066086bb20e33dc61d688
Ammad <ammadsdq@gmail.com>
2026-09-16 07:32:05 +0500

    Finalize production monitoring setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -170,10 +170,10 @@ resource "aws_eks_node_group" "main" {
     aws_subnet.public_b.id
   ]
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 3
+    desired_size = 4
     min_size     = 3
     max_size     = 4
   }
